a2fda9e05ede467c1a18d865d22a96543949d91e
cassidytrinh <cassidytrinh@gmail.com>
2018-06-21 15:18:40 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -30,6 +30,9 @@ end
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
+def all_keys(hash)
+  return hash.each_key {|key| puts key}
+end
 
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
